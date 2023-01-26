eb1ef15735639f56afc5c42406964fc1d4b4dc68
NymiKo <dimon.kabernik8@gmail.com>
2023-01-27 00:13:25 +0500

    added changing the question and checking the answers

--- app/src/main/java/com/easyprog/android/geomain/MainActivity.kt
+++ app/src/main/java/com/easyprog/android/geomain/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.easyprog.android.geomain
 
 import android.os.Bundle
-import android.view.Gravity
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,6 +11,20 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,17 +32,54 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {
-            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
-            toast.setGravity(Gravity.TOP, 0, 150)
-            toast.show()
+            checkAnswer(true)
         }
 
         falseButton.setOnClickListener {
-            val toast = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
-            toast.setGravity(Gravity.TOP, 0, 150)
-            toast.show()
+            checkAnswer(false)
         }
+
+        nextButton.setOnClickListener {
+            nextQuestion(true)
+            updateQuestion()
+        }
+
+        prevButton.setOnClickListener {
+            nextQuestion(false)
+            updateQuestion()
+        }
+
+        questionTextView.setOnClickListener {
+            nextQuestion(true)
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (correctAnswer == userAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+        nextQuestion(true)
+    }
+
+    private fun nextQuestion(correctAnswer: Boolean) {
+        currentIndex = (if (correctAnswer) currentIndex + 1 else currentIndex - 1) % questionBank.size
+        updateQuestion()
     }
 }
---
+++ app/src/main/java/com/easyprog/android/geomain/Question.kt
@@ -0,0 +1,5 @@
+package com.easyprog.android.geomain
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
